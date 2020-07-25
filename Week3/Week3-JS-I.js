6a84a99dbd648c84a5bb5acbb197d47107ad0334
HunterJuneau <huntlee17@gmail.com>
2020-07-25 13:15:34 -0500

    more fixes

--- Week3/Week3-JS-I.js
+++ Week3/Week3-JS-I.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 //In these first 6 questions, replace `null` with the answer
 
@@ -27,152 +27,153 @@ const newModulo = 21 % 5 === 4.125;
 
 // 7
 function returnString(str) {
-  //simply return the string provided: str
-  return str
+	//simply return the string provided: str
+	return str;
 }
 
 // 8
 function add(x, y) {
-  // x and y are numbers
-  // add x and y together and return the value
-  // code here
-  return x + y
+	// x and y are numbers
+	// add x and y together and return the value
+	// code here
+	return x + y;
 }
 
 // 9
 function subtract(x, y) {
-  // subtract y from x and return the value
-  // code here
-  return x - y
+	// subtract y from x and return the value
+	// code here
+	return x - y;
 }
 
 // 10
 function multiply(x, y) {
-  // multiply x by y and return the value
-  // code here
-  return x * y
+	// multiply x by y and return the value
+	// code here
+	return x * y;
 }
 
 // 11
 function divide(x, y) {
-  // divide x by y and return the value
-  // code here
-  return x / y
+	// divide x by y and return the value
+	// code here
+	return x / y;
 }
 
 // 12
 function areEqual(x, y) {
-  // return true if x and y are the same
-  // otherwise return false
-  // code here
-  return x === y
+	// return true if x and y are the same
+	// otherwise return false
+	// code here
+	return x === y;
 }
 
 // 13
 function areSameLength(str1, str2) {
-  // return true if the two strings have the same length
-  // otherwise return false
-  // code here
-  return str1.length === str2.length
+	// return true if the two strings have the same length
+	// otherwise return false
+	// code here
+	return str1.length === str2.length;
 }
 
 // 14
 function lessThanNinety(num) {
-  // return true if the function argument: num , is less than ninety
-  // otherwise return false
-  // code here
-  return num < 90
+	// return true if the function argument: num , is less than ninety
+	// otherwise return false
+	// code here
+	return num < 90;
 }
 
 // 15
 function greaterThanFifty(num) {
-  // return true if num is greater than fifty
-  // otherwise return false
-  // code here
-  return num > 50
+	// return true if num is greater than fifty
+	// otherwise return false
+	// code here
+	return num > 50;
 }
 
 // 16
 function getRemainder(x, y) {
-  // return the remainder from dividing x by y
-  // code here
-  return x % y
+	// return the remainder from dividing x by y
+	// code here
+	return x % y;
 }
 
 // 17
 function isEven(num) {
-  // return true if num is even
-  // otherwise return false
-  // code here
-  return num % 2 === 0
+	// return true if num is even
+	// otherwise return false
+	// code here
+	return num % 2 === 0;
 }
 
 // 18
 function isOdd(num) {
-  // return true if num is odd
-  // otherwise return false
-  // code here
-  return num % 2 !== 0
+	// return true if num is odd
+	// otherwise return false
+	// code here
+	return num % 2 !== 0;
 }
 
 // 19
 function square(num) {
-  // square num and return the new value
-  // hint: NOT square root!
-  // code here
-  return num * num
+	// square num and return the new value
+	// hint: NOT square root!
+	// code here
+	return num * num;
 }
 
 // 20
 function cube(num) {
-  // cube num and return the new value
-  // code here
-  return num * num * num
+	// cube num and return the new value
+	// code here
+	return num * num * num;
 }
 
 // 21
 function raiseToPower(num, exponent) {
-  // raise num to whatever power is passed in as exponent
-  // code here
-  return Math.pow(num, exponent)
+	// raise num to whatever power is passed in as exponent
+	// code here
+	return Math.pow(num, exponent);
 }
 
 // 22
 function roundNumber(num) {
-  // round num and return it
-  // code here
-  return Math.round(num)
+	// round num and return it
+	// code here
+	return Math.round(num);
 }
 
 // 23
 function roundUp(num) {
-  // round num up and return it
-  // code here
-  return Math.ceil(num)
+	// round num up and return it
+	// code here
+	return Math.ceil(num);
 }
 
 // 24
 function addExclamationPoint(str) {
-  // add an exclamation point to the end of str and return the new string
-  // 'hello world' -> 'hello world!'
-  // code here
-  return str.concat('!')
+	// add an exclamation point to the end of str and return the new string
+	// 'hello world' -> 'hello world!'
+	// code here
+	return str.concat('!');
 }
 
 // 25
 function combineNames(firstName, lastName) {
-  // return firstName and lastName combined as one string and separated by a space.
-  // 'NSS', 'Bootcamp' -> 'NSS Bootcamp'
-  // code here
-  return firstName.concat(' ', lastName)
+	// return firstName and lastName combined as one string and separated by a space.
+	// 'NSS', 'Bootcamp' -> 'NSS Bootcamp'
+	// code here
+	return firstName.concat(' ', lastName);
 }
 
 // 26
 function getGreeting(name) {
-  // Take the name string and concatenate other strings onto it so it takes the following form:
-  // 'Sam' -> 'Hello Sam!'
-  // code here
-  return concat('Hello ', name, '!')
+	// Take the name string and concatenate other strings onto it so it takes the following form:
+	// 'Sam' -> 'Hello Sam!'
+	// code here
+	const start = 'Hello ';
+	return start.concat(name, '!');
 }
 
 // The next three questions will have you implement math area formulas.
@@ -180,14 +181,14 @@ function getGreeting(name) {
 
 // 27
 function getRectangleArea(length, width) {
-  // return the area of the rectangle by using length and width
-  // code here
-  return length * width
+	// return the area of the rectangle by using length and width
+	// code here
+	return length * width;
 }
 
 // 28
 function getTriangleArea(base, height) {
-  // return the area of the triangle by using base and height
-  // code here
-  return base * height / 2
+	// return the area of the triangle by using base and height
+	// code here
+	return (base * height) / 2;
 }
